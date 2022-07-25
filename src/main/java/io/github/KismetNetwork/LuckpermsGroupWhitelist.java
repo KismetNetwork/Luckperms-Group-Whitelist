e94b5bcc54ebf61ccbad66d82f5c2e7b803afe29
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2022-07-25 03:30:02 +0200

    Fetch lp groups + get server name

--- src/main/java/io/github/KismetNetwork/LuckpermsGroupWhitelist.java
+++ src/main/java/io/github/KismetNetwork/LuckpermsGroupWhitelist.java
@@ -8,15 +8,18 @@ import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.NodeType;
+import net.luckperms.api.node.types.InheritanceNode;
 import org.slf4j.Logger;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Plugin(id = "luckpermsgroupwhitelist", name = "Luckperms Group Whitelist", version = "0.1.0-SNAPSHOT",
         url = "https://github.com/KismetNetwork/Luckperms-Group-Whitelist", description = "Only allow a certain luckperms group to join a server ", authors = {"AI-nsley69"})
@@ -42,13 +45,19 @@ public class LuckpermsGroupWhitelist {
     // onServerPreConnect event to check if the player is in the group it needs to be able to connect
     @Subscribe(order = PostOrder.EARLY)
     public static void onServerPreConnect(ServerPreConnectEvent event) {
-        // Get the player, server & luckperms user
+        // Get the player object
         Player player = event.getPlayer();
-        Optional<ServerConnection> server = player.getCurrentServer();
+        // Get the server and then the name from the server
+        RegisteredServer server = event.getOriginalServer();
+        String serverName = server.getServerInfo().getName();
+        // Get the luckperms user & group
         User user = luckPerms.getPlayerAdapter(Player.class).getUser(player);
+        Set<String> groups = user.getNodes(NodeType.INHERITANCE).stream()
+                .map(InheritanceNode::getGroupName)
+                .collect(Collectors.toSet());
         // TODO: check if server is in config & player is allowed
         /*
-        Component kickMessage = Component.text("You are not authorized on this server!");
+        Component kickMessage = Component.text("You are not authorized to connect to this server!");
         player.disconnect(kickMessage);
         */
     }
